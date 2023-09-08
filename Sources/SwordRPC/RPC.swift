d67f73a5b62289f64b4633e32ea5e010ec9904f5
Rushifaaa <jakubgencer@protonmail.com>
2023-09-08 22:20:19 +0200

    Fixed Buffer length error

--- Sources/SwordRPC/RPC.swift
+++ Sources/SwordRPC/RPC.swift
@@ -28,7 +28,7 @@ extension SwordRPC {
   func send(_ msg: String, _ op: OP) throws {
     let payload = msg.data(using: .utf8)!
 
-    var buffer = UnsafeMutableRawBufferPointer.allocate(byteCount: payload.count, alignment: 8)
+    var buffer = UnsafeMutableRawBufferPointer.allocate(byteCount: payload.count + 8, alignment: 0)
 
     defer { buffer.deallocate() }
 
